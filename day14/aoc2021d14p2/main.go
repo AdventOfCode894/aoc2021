cb0e34a69330571c27a7b3b68faaa50fcb0704e9
AdventOfCode894 <95511313+AdventOfCode894@users.noreply.github.com>
2021-12-14 21:59:43 -0500

    Day 14: much nicer p2 solution

--- day14/aoc2021d14p2/main.go
+++ day14/aoc2021d14p2/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
-	"sort"
+	"math"
+	"unicode/utf8"
 
 	"github.com/AdventOfCode894/aoc2021/internal/aocio"
 	"github.com/AdventOfCode894/aoc2021/internal/aocmain"
@@ -13,98 +15,68 @@ func main() {
 }
 
 func solvePuzzle(r io.Reader) (int, error) {
-	// This is really bad dynamic programming. Better solution: day 6 with fish named after digraphs!
-
 	pr := aocio.NewPuzzleReader(r)
-	rules := make(map[rune]map[rune]rune)
+	rules := make(map[string]rune)
 	pr.NextLine()
-	polymerTemplate := []rune(pr.LineString())
+	polymerTemplate := pr.LineString()
 	if err := pr.Err(); err != nil {
 		return 0, err
 	}
 	for pr.NextNonEmptyLine() {
 		tr := pr.LineTokenReader()
-		first, _ := tr.NextRune()
-		second, _ := tr.NextRune()
-		tr.ConsumeString(" -> ")
+		bigram, _ := tr.NextString(' ')
+		if len(bigram) != 2 {
+			return 0, fmt.Errorf("rule did not start with a bigram: %s", bigram)
+		}
+		tr.ConsumeString("-> ")
 		insert, _ := tr.NextRune()
 		tr.ConsumeEOL()
-		if _, seenFirst := rules[first]; !seenFirst {
-			rules[first] = make(map[rune]rune)
-		}
-		rules[first][second] = insert
+		rules[bigram] = insert
 	}
 	if err := pr.Err(); err != nil {
 		return 0, err
 	}
 
-	subQuantities := make(map[uint]map[rune]map[rune]map[rune]uint)
-
-	const maxSteps = 40
-	quantities := make(map[rune]uint)
+	bigramInstances := make(map[string]uint)
 	for i := 0; i < len(polymerTemplate)-1; i++ {
-		first := polymerTemplate[i]
-		second := polymerTemplate[i+1]
-		recordQuantitiesFor(first, second, maxSteps, rules, subQuantities)
-		for r, c := range subQuantities[maxSteps][first][second] {
-			quantities[r] += c
+		bigramInstances[polymerTemplate[i:i+2]]++
+	}
+
+	for step := 0; step < 40; step++ {
+		newInstances := make(map[string]uint)
+		for bigram, count := range bigramInstances {
+			insertion, hasRule := rules[bigram]
+			if !hasRule {
+				newInstances[bigram] = count
+				continue
+			}
+			bigramRunes := []rune(bigram)
+			first, second := bigramRunes[0], bigramRunes[1]
+			newInstances[string(first)+string(insertion)] += count
+			newInstances[string(insertion)+string(second)] += count
 		}
-		quantities[second]--
+		bigramInstances = newInstances
 	}
-	quantities[polymerTemplate[len(polymerTemplate)-1]]++
 
-	var polymers []rune
-	for r := range quantities {
-		polymers = append(polymers, r)
+	letterInstances := make(map[rune]uint)
+	for bigram, count := range bigramInstances {
+		l := []rune(bigram)[0]
+		letterInstances[l] += count
 	}
-	sort.Slice(polymers, func(i, j int) bool {
-		return quantities[polymers[i]] < quantities[polymers[j]]
-	})
+	lastLetter, _ := utf8.DecodeLastRuneInString(polymerTemplate)
+	letterInstances[lastLetter]++
 
-	leastCommon := quantities[polymers[0]]
-	mostCommon := quantities[polymers[len(polymers)-1]]
-	answer := mostCommon - leastCommon
+	minLetterCount := uint(math.MaxUint)
+	maxLetterCount := uint(0)
+	for _, count := range letterInstances {
+		if count < minLetterCount {
+			minLetterCount = count
+		}
+		if count > maxLetterCount {
+			maxLetterCount = count
+		}
+	}
 
+	answer := maxLetterCount - minLetterCount
 	return int(answer), nil
-}
-
-func recordQuantitiesFor(first rune, second rune, steps uint, rules map[rune]map[rune]rune, quantities map[uint]map[rune]map[rune]map[rune]uint) {
-	if _, ok := quantities[steps]; !ok {
-		quantities[steps] = make(map[rune]map[rune]map[rune]uint)
-	}
-	if _, ok := quantities[steps][first]; !ok {
-		quantities[steps][first] = make(map[rune]map[rune]uint)
-	}
-	if _, alreadyDone := quantities[steps][first][second]; alreadyDone {
-		return
-	}
-
-	quantities[steps][first][second] = make(map[rune]uint)
-	if steps < 1 {
-		quantities[steps][first][second][first]++
-		quantities[steps][first][second][second]++
-		return
-	}
-	if _, ok := rules[first]; !ok {
-		quantities[steps][first][second][first]++
-		quantities[steps][first][second][second]++
-		return
-	}
-	insert, ok := rules[first][second]
-	if !ok {
-		quantities[steps][first][second][first]++
-		quantities[steps][first][second][second]++
-		return
-	}
-
-	recordQuantitiesFor(first, insert, steps-1, rules, quantities)
-	recordQuantitiesFor(insert, second, steps-1, rules, quantities)
-	quantities[steps][first][second] = make(map[rune]uint)
-	for r, c := range quantities[steps-1][first][insert] {
-		quantities[steps][first][second][r] += c
-	}
-	for r, c := range quantities[steps-1][insert][second] {
-		quantities[steps][first][second][r] += c
-	}
-	quantities[steps][first][second][insert]--
 }
